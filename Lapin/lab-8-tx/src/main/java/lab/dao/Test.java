e48c27f22d01e3d250602641ec8866cd8ba01e8e
Konstantin Kolesov <Konstantin_Kolesov@epam.com>
2016-04-21 23:28:01 +0200

    start

---
+++ Lapin/lab-8-tx/src/main/java/lab/dao/Test.java
@@ -0,0 +1,7 @@
+package lab.dao;
+
+/**
+ * Created by Konstantin on 11.04.2016.
+ */
+public class Test {
+}
---
+++ Lapin/lab-8-tx/src/main/java/lab/service/CountryServiceImpl.java
@@ -0,0 +1,85 @@
+package lab.service;
+
+import java.util.List;
+
+import lab.dao.CountryDao;
+import lab.model.Country;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+//@Repository is more convenient declaration for such a class than general @Service
+@Repository
+public class CountryServiceImpl implements CountryService {
+
+	
+	@Autowired
+	private CountryDao countryDao;
+
+	@Transactional(propagation = Propagation.REQUIRED)
+	public List<Country> getAllCountriesInsideTransaction(
+			Propagation propagation) {
+		if (Propagation.REQUIRED.equals(propagation)) {
+			return getAllCountriesRequired();
+		} else if (Propagation.REQUIRES_NEW.equals(propagation)) {
+			return getAllCountriesRequiresNew();
+		} else if (Propagation.SUPPORTS.equals(propagation)) {
+			return getAllCountriesSupports();
+		} else if (Propagation.NEVER.equals(propagation)) {
+			return getAllCountriesNever();
+		} else if (Propagation.MANDATORY.equals(propagation)) {
+			return getAllCountriesMandatory();
+		} else if (Propagation.NOT_SUPPORTED.equals(propagation)) {
+			return getAllCountriesNotSupported();
+		} else {
+			return getAllCountries();
+		}
+	}
+	@Transactional(propagation = Propagation.REQUIRED)
+	public List<Country> getAllCountriesRequired() {
+		return countryDao.getCountryList();
+	}
+
+	@Transactional(propagation = Propagation.REQUIRES_NEW)
+	public List<Country> getAllCountriesRequiresNew() {
+		return countryDao.getCountryList();
+	}
+
+	@Transactional(propagation = Propagation.SUPPORTS)
+	public List<Country> getAllCountriesSupports() {
+		return countryDao.getCountryList();
+	}
+
+	@Transactional(propagation = Propagation.NEVER)
+	public List<Country> getAllCountriesNever() {
+		return countryDao.getCountryList();
+	}
+
+	@Transactional(propagation = Propagation.MANDATORY)
+	public List<Country> getAllCountriesMandatory() {
+		return countryDao.getCountryList();
+	}
+
+	@Transactional(propagation = Propagation.NOT_SUPPORTED)
+	public List<Country> getAllCountriesNotSupported() {
+		return countryDao.getCountryList();
+	}
+
+	@Transactional(propagation = Propagation.REQUIRED)
+	public List<Country> getAllCountries() {
+		return countryDao.getCountryList();
+	}
+
+	@Transactional(propagation = Propagation.REQUIRED)
+	public CountryDao getCountryDao() {
+		return countryDao;
+	}
+
+	@Transactional(propagation = Propagation.REQUIRED)
+	public void setCountryDao(CountryDao countryDao) {
+		this.countryDao = countryDao;
+	}
+
+}
